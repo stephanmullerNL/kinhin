4c96165e1d1d390617f8102ce97af51acd9a95f7
Stephan Muller <stephan.muller@rabobank.nl>
2020-03-17 00:49:22 +0100

    cleanup main js file

--- src/showRoutes.js
+++ src/showRoutes.js
@@ -1,27 +1,23 @@
 async function initMap() {
-    const netherlands = { lat: 52.208, lng: 5.525 };
+    const netherlands = { lat: 52.2, lng: 5.5 };
     const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 8,
         center: netherlands
     });
 
-    const data = await (await fetch('gpx/129-de-vuursche.gpx')).text();
-    console.log({ data });
-
-    var domParser = new DOMParser();
-    // Use it to turn your xmlString into an XMLDocument
-    var xmlDoc = domParser.parseFromString(data, 'application/xml');
-
+    const xmlDoc = await fetchXml('gpx/129-de-vuursche.gpx');
     const parser = new GPXParser(xmlDoc, map);
-    window.parser = parser;
 
     parser.setTrackColour('#0289d1'); // Set the track line colour
-    // parser.setTrackWidth(5); // Set the track line width
-    // parser.setMinTrackPointDelta(0.001); // Set the minimum distance between track points
     parser.addTrackpointsToMap(); // Add the trackpoints
-    console.log(parser.route, parser.segment);
 
     parser.segment.addListener('click', function() {
         parser.centerAndZoom(xmlDoc);
     });
+}
+
+async function fetchXml(uri) {
+    const data = await (await fetch(uri)).text();
+    var domParser = new DOMParser();
+    return domParser.parseFromString(data, 'application/xml');
 }
